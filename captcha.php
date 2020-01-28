c9219dbbeef51b17d559516e4f22fa3f94d45f7a
Nicolas <nicolas.fouchard94370@gmail.com>
2020-01-28 10:59:12 +0100

    ADD captcha.php with fonts

---
+++ captcha.php
@@ -0,0 +1,27 @@
+<?php
+
+		session_start();
+
+		header('Content-type:image/png');
+		$_SESSION['captcha'] = mt_rand(1000,9999);
+
+		$font_number = mt_rand(1,20);
+
+		$img = imagecreate(100, 30);
+
+		$font  = 'fonts/police(';
+		$font .= $font_number;
+		$font .= ').ttf';
+
+		$background = imagecolorallocate($img, 255, 255, 255);
+
+		$textcolor = imagecolorallocate($img, 0, 0, 0);
+
+		imagettftext($img, 20, 0, 20, 25, $textcolor, $font, $_SESSION['captcha']);
+		//imagettftext(image, size, angle, x, y, color, fontfile, text)
+
+
+		imagejpeg($img);
+		imagedestroy($img);
+
+?>
